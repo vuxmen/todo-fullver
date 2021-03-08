90316546ea9ec775e856cf9e9bd44fccbc11a671
vuxmen <imac@IMACs-iMac.local>
2021-03-09 00:23:59 +0700

    update2

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,52 @@
 import React from 'react';
+import style from './Signup.module.css';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import { useHistory } from 'react-router-dom';
 
 export default function Signup () {
+    const history = useHistory();
+    const handleGoBackLogin = () => history.push('/'); 
+
     return (
-        <div>abc</div>
+        <div className = {style.signUp}>
+            <div className = {style.header}>
+                <ArrowBackIosIcon className = {style.backArrow} onClick = {handleGoBackLogin}/>
+                <span>Back to Login</span>
+            </div>
+            <div className = {style.container}>
+                <div class = {style.title}>
+                    <h2>Fill your Information</h2>
+                </div>
+                
+                <form >
+                    <div className = {style.element}>
+                        <label htmlFor="name">Name: </label>
+                        <input type="text"/>
+                    </div>
+                    <div className = {style.element}>
+                        <label htmlFor="username">Username: </label>
+                        <input type="text"/>
+                    </div>
+                    <div className = {style.element}>
+                        <label htmlFor="password">Password: </label>
+                        <input type="text"/>
+                    </div>
+                    <div className = {style.element}>
+                        <label htmlFor="phoneNumber">PhoneNumber: </label>
+                        <input type="text"/>
+                    </div>
+                    <div className = {style.element}>
+                        <label htmlFor="email">Email: </label>
+                        <input type="text"/>
+                    </div>
+                    <div className = {style.element}>
+                        <label htmlFor="birthday">Birthday: </label>
+                        <input type="date"/>
+                    </div>
+                    <button type = "submit">Sign Up</button>
+                </form>
+                
+            </div>
+        </div>
     );
 }
