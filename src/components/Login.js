343d4e82c45a4678827d10c1f8e5c3a6425703d3
vuxmen <m@ms-MacBook-Pro.local>
2021-03-09 20:26:48 +0700

    update4

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,23 +2,43 @@ import React from 'react';
 import style from './Login.module.css';
 import { Link } from 'react-router-dom';
 import { useHistory } from 'react-router-dom';
+import { useFormik } from 'formik';
+import Validation from '../Validation';
 
 export default function Login () {
     const history = useHistory();
-    const handleSubmit = () => {
-        //Check some Condition before move to TotoTask
-        history.push('/TodoTask');
-    }
+    const formik = useFormik({
+        initialValues: {
+            username: '',
+            password: ''
+        },
+        validationSchema: Validation,
+        onSubmit: () => {
+            
+        }
+    });
+    
 
     return (
         <div className = {style.login} >
             <div className = {style.login_container}>
                 <h2>Welcome Todo App</h2>
-                <form className = {style.form} >
-                    <input type="text" placeholder= "   Type ur username..." />
-                    <input type="text" placeholder= "   Type ur password..." />
-                    <button type = "submit" onClick = {handleSubmit}>Login</button>
+                <form className = {style.form} onSubmit = {formik.handleSubmit} >
+                    <input name = "username" type="text" placeholder= "   Type ur username..."
+                    value = {formik.values.username}
+                    onChange = {formik.handleChange}
+                     />
+                    <input name = "password" type="text" placeholder= "   Type ur password..."
+                    value = {formik.values.password} 
+                    onChange = {formik.handleChange}
+                     />
+                    <button type = "submit">Login</button>
                     <Link to ="/Signup" className = {style.linkSignup}>Don't have an account ? Sign up here</Link>
+                    {
+                        Object.values(formik.errors).map(error => (
+                            <div>{error}</div>
+                        ))
+                    }
                 </form>
                 
             </div>
